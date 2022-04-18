e2683b730cb7b16f50e18ee3e96c7beb2d4d4de9
leflocju <julian.lefloch@reseau.eseo.fr>
2022-04-18 12:19:54 +0200

    :boom: V1 finie

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -8,6 +8,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.print("Application demarrée, vous pouvez utiliser le serveur !");
+		System.out.print("Application demarrée, vous pouvez donc utiliser l'API REST à votre convenance !");
 	}
 }
